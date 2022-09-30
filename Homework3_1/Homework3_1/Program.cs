4fcd89e5e894f358ae70cf174dd799bf6dd20f4d
Игорь <@DESKTOP-B894K3L>
2022-09-30 23:42:06 +0300

    15

---
+++ Homework3_1/Homework3_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Quadrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square mf = new Square(5);
+            mf.DrawSquare(); 
+        }
+        class Quadro
+        {
+            public int Base { get; set; }
+        }
+        interface ISimpleNAngle 
+        {
+            int Base { get; set; }
+            int BaseSideAngle { get; set; }
+            int NumberSide { get; set; }
+        }
+        class Square : ISimpleNAngle
+        {
+            public int Base { get; set; }
+            public int BaseSideAngle { get; set; }
+            public int NumberSide { get; set; }
+            public Square(int numberSide)
+            {
+                NumberSide = numberSide;
+                BaseSideAngle = 360 / numberSide;
+            }
+            public void DrawSquare()
+            {
+                char[] drawChar = new char[8] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                for (int i = 1; i < drawChar.Length; i++)
+                {
+                    for (int j = 0; j < drawChar.Length; j++)
+                    {
+                        drawChar[j] = '*';
+                    }
+                    Console.WriteLine(drawChar);
+                }
+            }
+    }   }
+}
