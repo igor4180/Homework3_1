84fabde8fee73acf4df87c523b9007171acea515
Игорь <@DESKTOP-B894K3L>
2022-10-04 22:25:43 +0300

    добавлены файлы

---
+++ Cards/Cards/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Numerics;
+
+//Выводит пустую консоль
+namespace Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Play();
+        }
+    }
+    public class Game
+    {
+        public List<Player> Players { get; set; }
+        public List<Karta> Cards { get; set; }
+
+        public Game()
+        {
+            const int cardStart = 6;
+            const int cardEnd = 15;
+            const int cardTypeStart = 0;
+            const int cardTypeEnd = 4;
+
+            Random random = new Random();
+
+            Players = new List<Player>();
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            Cards = new List<Karta>();
+            List<Karta> list = new List<Karta>();
+
+            for (int i = cardStart; i < cardEnd; i++)
+            {
+                for (int j = cardTypeStart; j < cardTypeEnd; j++)
+                {
+                    list.Add(new Karta { CardLevel = i, CardType = j });
+                }
+            }
+
+
+            //shuffle
+            for (int i = 0; i < 36; i++)
+            {
+                int randomIndex = random.Next(0, list.Count);
+                Karta card = list.ElementAt(randomIndex);
+                list.RemoveAt(randomIndex);
+                Cards.Add(card);
+            }
+
+            //cards for player1
+            for (int i = 0; i < 18; i++)
+            {
+                int randomIndex = random.Next(0, Cards.Count);
+                Karta card = Cards.ElementAt(randomIndex);
+                Cards.RemoveAt(randomIndex);
+                player1.Cards.Add(card);
+            }
+
+            //cards for player2
+            player2.Cards.InsertRange(0, Cards);
+
+            Players.Add(player1);
+            Players.Add(player2);
+
+        }
+        public void Play()
+        {
+            bool isGameOver = false;
+            do
+            {
+                Dictionary<Player, Karta> playerCard = new Dictionary<Player, Karta>();
+
+
+                foreach (var player in Players)
+                {
+                    playerCard.Add(player, player.Cards.First());
+                    player.Cards.RemoveAt(0);
+                }
+
+                int maxCard = playerCard.Max(card => card.Value.CardLevel);
+
+                Player firstWinPlayer = (playerCard.First(card => card.Value.CardLevel == maxCard).Key);
+
+                foreach (var player1 in playerCard)
+                {
+                    firstWinPlayer.Cards.Add(player1.Value);
+                }
+
+
+                foreach (var player in Players)
+                {
+                    if (player.Cards.Count == 36)
+                    {
+                        isGameOver = true;
+                        Console.WriteLine(player.Name + " WON!");
+                        break;
+                    }
+                }
+
+            } while (!isGameOver);
+        }
+    }
+    public class Player
+    {
+        public static int playerCount = 0;
+        public string Name { get; set; }
+        public List<Karta> Cards { get; set; }
+        public Player()
+        {
+            playerCount++;
+            Name = "Player #" + playerCount;
+            Cards = new List<Karta>();
+        }
+    }
+    public class Karta
+    {
+        public int CardType { get; set; }
+        public int CardLevel { get; set; }
+    }
+}
