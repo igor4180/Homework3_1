a9feb455ded250ace9771f6d85d61966c9078076
Игорь <@DESKTOP-B894K3L>
2022-10-05 22:50:12 +0300

    добавлены файлы

---
+++ arifmetika/arifmetika/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace arifmetika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double result;
+            char op;
+
+            Console.WriteLine("Введите первое число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите арифметическое действие: ");
+            op = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (op == '+')
+            {
+                result = a + b;
+                Console.WriteLine(+a + "+" + b + "=" + result);
+            }
+            else if (op == '-')
+            {
+                result = a - b;
+                Console.WriteLine(+a + "-" + b + "=" + result);
+            }
+            else if (op == '*')
+            {
+                result = a * b;
+                Console.WriteLine(+a + "*" + b + "=" + result);
+            }
+            else if (op == '/')
+            {
+                result = a / b;
+                Console.WriteLine(+a + "/" + b + "=" + result);
+            }
+            else
+            {
+                Console.WriteLine("Неправильно! Попытайтесь заново.");
+            }
+
+        }
+    }
+
+}
+
+
