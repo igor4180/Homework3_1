8edc88668d2494580599d11aaed4bcde2f0c86a0
Игорь <@DESKTOP-B894K3L>
2022-10-13 23:08:31 +0300

    125

---
+++ Books/Books/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace Library
+{
+    internal class Book
+    {
+        private int v1;
+        private string v2;
+        private string v3;
+        private string v4;
+        private int v5;
+        private DateTime today;
+
+        public Book(int v1, string v2, string v3, string v4, int v5, DateTime today)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.today = today;
+        }
+    }
+}
---
+++ Books/Books/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+//выводит пустой экран
+
+namespace Library
+{
+    class Program
+    {
+        private static List<Book> books;
+
+        static void Main(string[] args)
+        {
+            InitializeBookList();
+            FindByPublisher();
+
+
+            Console.ReadKey();
+        }
+
+        private static void FindByPublisher()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void FindByPublisher(string publisher)
+        {
+            books.FindAll(book => book.Publisher == publisher).
+                  ForEach(book => Console.WriteLine(book.ID));
+        }
+
+        private static void InitializeBookList()
+        {
+            books = new List<Book>();
+            books.AddRange(new[]
+            {
+             new Book(0, "Автор 1", "Книга 1", "Издательство 1", 150, DateTime.Today),
+             new Book(1, "Автор 1", "Книга 2", "Издательство 2", 1150, DateTime.Today),
+             new Book(2, "Автор 2", "Книга 1", "Издательство 2", 150, DateTime.Today),
+             new Book(3, "Автор 3", "Книга 1", "Издательство 3", 150, DateTime.Today)
+            });
+        }
+        class Book
+        {
+            public int ID { get; set; }
+            public string Author { get; set; }
+            public string Name { get; set; }
+            public string Publisher { get; set; }
+            public DateTime PublicationYear { get; set; }
+            public int PageNumber { get; set; }
+
+            public Book
+                (int id,
+                string author,
+                string name,
+                string publisher,
+                int pageNumber,
+                DateTime year)
+            {
+                this.ID = id;
+                this.Author = author;
+                this.Name = name;
+                this.Publisher = publisher;
+                this.PageNumber = pageNumber;
+                this.PublicationYear = year;
+            }
+        }
+    }
+}
