ace07a61e38ca85c4f597ce637b204532d5e9e0c
Игорь <@DESKTOP-B894K3L>
2022-11-15 23:34:46 +0300

    123

---
+++ Calendar1/Calendar1/Form1.cs
@@ -0,0 +1,21 @@
+using System.Windows.Forms;
+
+namespace Calendar1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        //не пойму, что писать в коде
+    }
+}   
