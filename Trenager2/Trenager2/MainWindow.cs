4894469ec3bad3a83610e414005d39ecaf0adc15
Игорь <@DESKTOP-B894K3L>
2022-11-26 22:57:41 +0300

    149

---
+++ Trenager2/Trenager2/MainWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trenager2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    internal sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string textForTask = "";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            speed.Text = "0";
+            fails.Text = "0";
+        }
+
+        private void difficult_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            labelDifficult.Text = ((int)difficult.Value).ToString();
+            taskText.Text = TextGenerator.GetTextByDiffLvl((int)difficult.Value);
+        }
+
+    }
+}
---
+++ Trenager2/Trenager2/TextGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trenager2
+{
+    internal class TextGenerator
+    {     
+            static char[] charsLevelZero = new char[] { 'r', 't', 'y', 'u', 'f', 'g', ' ', 'h', 'j', 'v', 'b', 'n', 'm' };
+            static char[] charsLevelOne = new char[] { 'r', 't', 'y', 'u', 'f', 'g', ' ', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8' };
+            static char[] charsLevelTwo = new char[] { 'r', 't', 'y', 'u', 'f', 'g', ' ', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9' };
+            static char[] charsLevelThree = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x' };
+            static char[] charsLevelFour = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x', 'q', 'a', 'z' };
+            static char[] charsLevelFive = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x', 'o', 'l', '.' };
+            static char[] charsLevelSix = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x', 'o', 'l', '.', 'p', ';', '/' };
+            static char[] charsLevelSeven = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x', 'o', 'l', '.', 'p', ';', '/', ']' };
+            static char[] charsLevelEight = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x', 'o', 'l', '.', 'p', ';', '/', ']', '3' };
+            static char[] charsLevelNine = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm',  '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x', 'o', 'l', '.', 'p', ';', '/', ']', '2' };
+            static char[] charsLevelTen = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9', 'w', 's', 'x', 'o', 'l', '.', 'p', ';', '/', ']', '1', '`' };
+            static public string GetTextByDiffLvl(int diffLvl)
+            {
+                char[] currentLevelMassive;
+
+                switch (diffLvl)
+                {
+                    case 1: currentLevelMassive = charsLevelZero; break;
+                    case 2: currentLevelMassive = charsLevelOne; break;
+                    case 3: currentLevelMassive = charsLevelTwo; break;
+                    default: currentLevelMassive = charsLevelZero; break;
+                }
+                return GenerateText(currentLevelMassive);
+            }
+
+            static string GenerateText(char[] chars)
+            {
+                string resultText = "";
+                Random rnd = new Random();
+                for (int i = 0; i < 70; i++)
+                {
+                    resultText += chars[rnd.Next(0, chars.Length)];
+                }
+
+                return resultText;
+            }
+
+        
+    }
+}
