fb1a64633cc6e5c0eac31654a4c1c278a8f5e22c
Игорь <@DESKTOP-B894K3L>
2023-03-01 21:25:29 +0300

    18

---
+++ NPLesson1Server/NPLesson1Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPLesson1Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            IPEndPoint point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+
+            try
+            {
+                socket.Connect(point);
+                byte[] buffer = new byte[1024];
+                int c;
+                do
+                {
+                    c = socket.Receive(buffer);
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer));
+                }
+                while (c > 0);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+        }
+    }
+}
