e058081b97959e348ddc8b105dde140e6b657320
Игорь <@DESKTOP-B894K3L>
2023-03-09 21:49:15 +0300

    16

---
+++ CodeMessageBox/CodeMessageBox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMessageBox
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint code, string attr1, string attr2);
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string attr1, string attr2, uint code = 0x0);
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero, "Игорь", "имя");
+            MessageBox(IntPtr.Zero, "Бесклеткин", "фамилия");
+            MessageBox(IntPtr.Zero, "студент", "должность");
+        }
+    }
+}
