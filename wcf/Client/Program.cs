60711cf92168231c259db5507c3cade24f950ca8
Игорь <@DESKTOP-B894K3L>
2023-03-26 22:33:03 +0300

    89

---
+++ wcf/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    [ServiceContract]
+    public interface IMessageService
+    {
+        [OperationContract]
+        string[] GetMessages();
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press any key to const...");
+            Console.ReadLine();
+            string uri = "net.tcp://localhost:6565/MessageService";
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+            var channel = new ChannelFactory<IMessageService>(binding);
+            var endpoint = new EndpointAddress(uri);
+            var proxy = channel.CreateChannel(endpoint);
+            var result = proxy?.GetMessages();
+            if (result != null)
+                result.ToList().ForEach(p =>
+                {
+                    Console.WriteLine(p);
+                });
+        }
+    }
+}
