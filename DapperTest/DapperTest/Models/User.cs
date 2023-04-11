27ce739693ec8e00f1468b0c5a61bd48086e885c
Игорь <@DESKTOP-B894K3L>
2023-04-11 23:31:21 +0300

    74

---
+++ DapperTest/DapperTest/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace DapperTest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Buyer { get; set; }
+        public string Countries { get; set; }
+        public string Cities { get; set; }
+        public int Section { get; set; }
+        public string Goods { get; set; }
+    }
+}
