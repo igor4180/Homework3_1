557438bcfa64a2f5d8895337361d1687f88e67f3
Igor Beskletkin <Beskletkin@gmail.com>
2023-04-27 21:28:25 +0300

    148

---
+++ DaugtherProcess/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TestHomeWork22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStartCalc
+            // 
+            this.buttonStartCalc.Location = new System.Drawing.Point(12, 40);
+            this.buttonStartCalc.Name = "buttonStartCalc";
+            this.buttonStartCalc.Size = new System.Drawing.Size(297, 208);
+            this.buttonStartCalc.TabIndex = 0;
+            this.buttonStartCalc.Text = "Нажмите для запуска блокнота";
+            this.buttonStartCalc.UseVisualStyleBackColor = true;
+            this.buttonStartCalc.Click += new System.EventHandler(this.buttonStartCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 296);
+            this.Controls.Add(this.buttonStartCalc);
+            this.Name = "Form1";
+            this.Text = "Процесс";
+            this.ResumeLayout(false);
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartCalc;
+    }
+}
+
---
+++ DaugtherProcess/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using System.Runtime.InteropServices;
+
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DagtherProcess 
+{
+    public partial class Form1 : Form
+    {
+        public class DllImportExample
+        {
+            [DllImport("User32.dll")]
+            public static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+        }
+        const uint MB_OK = 0x00000000;
+        const uint MB_INFORMATION = 64;
+        const uint MB_ICONERROR = 0x00000010;
+        private Process myProcess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void buttonStartCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myProcess = new Process();
+                myProcess.StartInfo.FileName = "notepad.exe";
+                myProcess.EnableRaisingEvents = true;
+                myProcess.Exited += MyProcess_Exited;
+                myProcess.Start();
+            }
+            catch (Exception ex)
+            {
+                DllImportExample.MessageBox(IntPtr.Zero, "ERROR","", MB_OK | MB_ICONERROR);
+            }
+
+        }
+
+        private void MyProcess_Exited(object sender, EventArgs e)
+        {
+            DllImportExample.MessageBox(IntPtr.Zero,$"Exit time: {myProcess.ExitTime}\nExit code: {myProcess.ExitCode}","", MB_OK | MB_INFORMATION);
+            myProcess.Close();
+        }
+    }
+}
